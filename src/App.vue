77f4e8011df8788e0f5457e322df0f67000a9d58
Hoaveth <hoaveth.gaming@gmail.com>
2023-07-10 14:06:03 +0800

    Initial files for school app

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <div id="nav">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/brazil">Brazil</RouterLink>
+      </div>
+    </div>
+  </header>
+
+  <div class="container">
+    <RouterView />
+  </div>
+</template>
